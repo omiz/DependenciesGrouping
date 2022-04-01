8e225be03c5f5b3d175384c6039b504b54690add
Omar Allaham <o.allaham@icloud.com>
2022-04-01 10:32:35 +0200

    added helper framework and package

---
+++ HelperFramework/HelperFramework/HelperFramework.h
@@ -0,0 +1,18 @@
+//
+//  HelperFramework.h
+//  HelperFramework
+//
+//  Created by Omar Allaham on 4/1/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelperFramework.
+FOUNDATION_EXPORT double HelperFrameworkVersionNumber;
+
+//! Project version string for HelperFramework.
+FOUNDATION_EXPORT const unsigned char HelperFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelperFramework/PublicHeader.h>
+
+
