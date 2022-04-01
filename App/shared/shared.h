08aa858fb2050755646a5a45b26f9416ae86d791
Omar Allaham <o.allaham@icloud.com>
2022-04-01 10:34:41 +0200

    added shared framework

---
+++ App/shared/shared.h
@@ -0,0 +1,18 @@
+//
+//  shared.h
+//  shared
+//
+//  Created by Omar Allaham on 4/1/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for shared.
+FOUNDATION_EXPORT double sharedVersionNumber;
+
+//! Project version string for shared.
+FOUNDATION_EXPORT const unsigned char sharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <shared/PublicHeader.h>
+
+
